561fffe80aa1104bc97770f49bc746c0f89e256b
ArnabKumarRoy02 <86621483+ArnabKumarRoy02@users.noreply.github.com>
2022-05-15 02:54:17 +0530

    Update HandTrackingMin.py

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -15,23 +15,23 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for ids, lm in enumerate(handLms.landmark):
-                # print(ids, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 print(ids, cx, cy)
-                # if ids == 4:
                 cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+#   cTime - checks the current time
+#   pTime - checks the previous time
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
 
+#   Prints the fps on the top left corner of the screen
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3,
                 (100, 100, 255), 3)
 
